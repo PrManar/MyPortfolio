9cb3a9ebb3050dd146d8569ea4fde0c12eebc7de
Manar <xmanardhawi@gmail.com>
2026-02-23 00:26:00 +0300

    create static folder and base.html file

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('certifications/', views.certifications, name='certifications'),
+    path('contact/', views.contact, name='contact'),
+]
